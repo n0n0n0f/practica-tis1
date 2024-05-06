2c1fc29691a0aba05571ed20205bd8f2178444e2
n0n0n0f <r4sl3xgod@gmail.com>
2024-05-06 16:48:27 +0700

    Негативная авторизация

--- cypress/e2e/Auth_negative.cy.js
+++ cypress/e2e/Auth_negative.cy.js
@@ -1,12 +1,13 @@
-describe('Авторизация', () => {
+
+describe('Авторизация негативная, логин', () => {
   it('Not-existent login test', () => {
     cy.fixture('cypressTest').then(data => { 
       cy.log('Переход на страницу авторизации')
-      cy.visit(data.main_url)
+      cy.visit(data.auth_url)
 
       cy.log('Ввод существующего логина')
       cy.get('.form-input--text')
-          .type(data.login)
+          .type(data.none_existent_login)
 
        cy.log('Ввод существующего пароля')
       cy.get('.form-input--password')
@@ -19,11 +20,12 @@ describe('Авторизация', () => {
   })
 })
 
-describe('Авторизация', () => {
+
+describe('Авторизация негативная, пароль', () => {
   it('Not-existent login test', () => {
     cy.fixture('cypressTest').then(data => { 
       cy.log('Переход на страницу авторизации')
-      cy.visit(data.main_url)
+      cy.visit(data.auth_url)
 
       cy.log('Ввод существующего логина')
       cy.get('.form-input--text')
@@ -31,7 +33,7 @@ describe('Авторизация', () => {
 
        cy.log('Ввод существующего пароля')
       cy.get('.form-input--password')
-          .type(data.password)
+          .type(data.none_existent_password)
         
        cy.log('Клик по кнопке войти')
       cy.get(':nth-child(3) > .button')
@@ -39,4 +41,3 @@ describe('Авторизация', () => {
     })
   })
 })
-
---
+++ cypress/e2e/regist.cy.js
@@ -0,0 +1,21 @@
+describe('Авторизация', () => {
+  it('Auth test', () => {
+    cy.fixture('cypressTest').then(data => { 
+      cy.log('Переход на страницу авторизации')
+      cy.visit(data.auth_url)
+
+      cy.log('Ввод существующего логина')
+      cy.get('.form-input--text')
+          .type(data.login)
+
+       cy.log('Ввод существующего пароля')
+      cy.get('.form-input--password')
+          .type(data.password)
+        
+       cy.log('Клик по кнопке войти')
+      cy.get(':nth-child(3) > .button')
+          .click()
+    })
+  })
+})
+
